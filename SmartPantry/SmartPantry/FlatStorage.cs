1a52c372d05243f7dca4e7dcdbcc547285ee1e36
Jacob Huber <jhuber11235@gmail.com>
2021-04-10 18:26:49 -0700

    initial flat storage stuff

---
+++ SmartPantry/SmartPantry/FlatStorage.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartPantry
+{
+    public class FlatStorage
+    {
+        // recipes
+
+        // food items
+        
+        // shopping list
+        
+        // user
+
+        public void SaveToFile(string username)
+        {
+            using (StreamWriter file = new StreamWriter(@Path.GetFullPath(Directory.GetCurrentDirectory()) + username + ".txt")
+            {
+                
+            }
+        }
+
+        public void ReadFromFile(string username)
+        {
+            using (StreamReader file = new StreamReader(Path.GetFullPath(Directory.GetCurrentDirectory()) + "\\" + username + ".txt"))
+            {
+                string line = file.ReadLine();
+                while (line != null)
+                {
+                    string[] lineArray = line.Split(',');
+
+
+                }
+            }
+        }
+    }
+}
