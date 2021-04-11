c518ffc98491c316aca5a3d92d222de7b5a19a3b
galvink2 <galvink2@gmail.com>
2021-04-11 00:45:18 -0700

    Moved the goofy buttons back.

--- SmartPantry/SmartPantry/NewRecipeForm.Designer.cs
+++ SmartPantry/SmartPantry/NewRecipeForm.Designer.cs
@@ -45,68 +45,61 @@ namespace SmartPantry
             // 
             // newRecipeDescriptionTB
             // 
-            this.newRecipeDescriptionTB.Location = new System.Drawing.Point(60, 231);
-            this.newRecipeDescriptionTB.Margin = new System.Windows.Forms.Padding(6);
+            this.newRecipeDescriptionTB.Location = new System.Drawing.Point(30, 120);
             this.newRecipeDescriptionTB.Multiline = true;
             this.newRecipeDescriptionTB.Name = "newRecipeDescriptionTB";
             this.newRecipeDescriptionTB.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.newRecipeDescriptionTB.Size = new System.Drawing.Size(526, 148);
+            this.newRecipeDescriptionTB.Size = new System.Drawing.Size(265, 79);
             this.newRecipeDescriptionTB.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.SystemColors.Window;
-            this.label1.Location = new System.Drawing.Point(54, 200);
-            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Location = new System.Drawing.Point(27, 104);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(427, 25);
+            this.label1.Size = new System.Drawing.Size(211, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Enter Description and Procedure if Desired:";
             // 
             // recipeNameTB
             // 
-            this.recipeNameTB.Location = new System.Drawing.Point(60, 113);
-            this.recipeNameTB.Margin = new System.Windows.Forms.Padding(6);
+            this.recipeNameTB.Location = new System.Drawing.Point(30, 59);
             this.recipeNameTB.Name = "recipeNameTB";
-            this.recipeNameTB.Size = new System.Drawing.Size(494, 31);
+            this.recipeNameTB.Size = new System.Drawing.Size(249, 20);
             this.recipeNameTB.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.ForeColor = System.Drawing.SystemColors.Window;
-            this.label2.Location = new System.Drawing.Point(54, 83);
-            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Location = new System.Drawing.Point(27, 43);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(240, 25);
+            this.label2.Size = new System.Drawing.Size(120, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "Enter Name of the Dish:";
             // 
             // ingredient1
             // 
-            this.ingredient1.Location = new System.Drawing.Point(60, 492);
-            this.ingredient1.Margin = new System.Windows.Forms.Padding(6);
+            this.ingredient1.Location = new System.Drawing.Point(30, 256);
             this.ingredient1.Name = "ingredient1";
-            this.ingredient1.Size = new System.Drawing.Size(256, 31);
+            this.ingredient1.Size = new System.Drawing.Size(130, 20);
             this.ingredient1.TabIndex = 4;
             // 
             // quantity1
             // 
-            this.quantity1.Location = new System.Drawing.Point(332, 492);
-            this.quantity1.Margin = new System.Windows.Forms.Padding(6);
+            this.quantity1.Location = new System.Drawing.Point(166, 256);
             this.quantity1.Name = "quantity1";
-            this.quantity1.Size = new System.Drawing.Size(102, 31);
+            this.quantity1.Size = new System.Drawing.Size(53, 20);
             this.quantity1.TabIndex = 5;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.ForeColor = System.Drawing.SystemColors.Window;
-            this.label3.Location = new System.Drawing.Point(54, 462);
-            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Location = new System.Drawing.Point(27, 240);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(175, 25);
+            this.label3.Size = new System.Drawing.Size(88, 13);
             this.label3.TabIndex = 6;
             this.label3.Text = "Ingredient Name:";
             // 
@@ -114,10 +107,9 @@ namespace SmartPantry
             // 
             this.label4.AutoSize = true;
             this.label4.ForeColor = System.Drawing.SystemColors.Window;
-            this.label4.Location = new System.Drawing.Point(326, 462);
-            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Location = new System.Drawing.Point(163, 240);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(98, 25);
+            this.label4.Size = new System.Drawing.Size(49, 13);
             this.label4.TabIndex = 7;
             this.label4.Text = "Quantity:";
             // 
@@ -125,9 +117,10 @@ namespace SmartPantry
             // 
             this.saveRecipeButton.BackColor = System.Drawing.Color.Silver;
             this.saveRecipeButton.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.saveRecipeButton.Location = new System.Drawing.Point(30, 422);
+            this.saveRecipeButton.Location = new System.Drawing.Point(30, 420);
+            this.saveRecipeButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.saveRecipeButton.Name = "saveRecipeButton";
-            this.saveRecipeButton.Size = new System.Drawing.Size(530, 87);
+            this.saveRecipeButton.Size = new System.Drawing.Size(265, 45);
             this.saveRecipeButton.TabIndex = 18;
             this.saveRecipeButton.Text = "Save Recipe!";
             this.saveRecipeButton.UseVisualStyleBackColor = false;
@@ -137,9 +130,10 @@ namespace SmartPantry
             // 
             this.addRecipeItemButton.BackColor = System.Drawing.Color.Silver;
             this.addRecipeItemButton.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.addRecipeItemButton.Location = new System.Drawing.Point(225, 254);
+            this.addRecipeItemButton.Location = new System.Drawing.Point(224, 253);
+            this.addRecipeItemButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.addRecipeItemButton.Name = "addRecipeItemButton";
-            this.addRecipeItemButton.Size = new System.Drawing.Size(150, 44);
+            this.addRecipeItemButton.Size = new System.Drawing.Size(75, 23);
             this.addRecipeItemButton.TabIndex = 19;
             this.addRecipeItemButton.Text = "Add Item";
             this.addRecipeItemButton.UseVisualStyleBackColor = false;
@@ -150,29 +144,27 @@ namespace SmartPantry
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Rage Italic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.label5.Location = new System.Drawing.Point(24, -1);
+            this.label5.Location = new System.Drawing.Point(12, -1);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(391, 68);
+            this.label5.Size = new System.Drawing.Size(199, 34);
             this.label5.TabIndex = 20;
             this.label5.Text = "Create New Recipe";
             // 
             // newRecipeItemLB
             // 
             this.newRecipeItemLB.FormattingEnabled = true;
-            this.newRecipeItemLB.ItemHeight = 25;
-            this.newRecipeItemLB.Location = new System.Drawing.Point(64, 567);
-            this.newRecipeItemLB.Margin = new System.Windows.Forms.Padding(6);
+            this.newRecipeItemLB.Location = new System.Drawing.Point(32, 295);
             this.newRecipeItemLB.Name = "newRecipeItemLB";
-            this.newRecipeItemLB.Size = new System.Drawing.Size(370, 204);
+            this.newRecipeItemLB.Size = new System.Drawing.Size(187, 108);
             this.newRecipeItemLB.TabIndex = 21;
-            //this.newRecipeItemLB.SelectedIndexChanged += new System.EventHandler(this.newRecipeItemLB_SelectedIndexChanged);
             // 
             // NewRecipeForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.ClientSize = new System.Drawing.Size(644, 973);
+            this.ClientSize = new System.Drawing.Size(322, 506);
             this.Controls.Add(this.newRecipeItemLB);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.addRecipeItemButton);
@@ -186,7 +178,6 @@ namespace SmartPantry
             this.Controls.Add(this.label1);
             this.Controls.Add(this.newRecipeDescriptionTB);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "NewRecipeForm";
             this.ResumeLayout(false);
             this.PerformLayout();
