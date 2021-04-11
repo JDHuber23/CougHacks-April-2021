ff11d62b96fab656ed67f3fd5f3be37e7a160927
Jacob Huber <jhuber11235@gmail.com>
2021-04-11 01:17:51 -0700

    Added default for in case user file doesn't exist

--- SmartPantry/SmartPantry/Program.cs
+++ SmartPantry/SmartPantry/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -17,34 +18,39 @@ namespace SmartPantry
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            /*User user = new User("dummy");
-
-            // fill user with info 
-            FoodItem food1 = new FoodItem("Bread", "Pantry", new DateTime(2020, 07, 11), 2);
-            FoodItem food2 = new FoodItem("Peanut Butter", "Pantry", new DateTime(2020, 07, 11), 1);
-            FoodItem food3 = new FoodItem("Jelly", "Refrigerator", new DateTime(2020, 07, 11), 1);
-            FoodItem food4 = new FoodItem("Chicken", "Freezer", new DateTime(2020, 07, 11), 4);
-
-
-            user.addFoodToKitchen(food1);
-            user.addFoodToKitchen(food2);
-            user.addFoodToKitchen(food3);
-            user.addFoodToKitchen(food4);
-
-            //Recipe
-            Dictionary<string, int> ingredients = new Dictionary<string, int>()
-            {
-                { "Bread", 2 },
-                {"Peanut Butter", 1 },
-                {"Jelly", 1 }
-            };
-
-            Recipe recipe = new Recipe("PB & J", ingredients, "These are the instructions.");
-
-            user.addRecipe(recipe);
-*/
             FlatStorage storage = new FlatStorage(null);
-            User user = storage.ReadFromFile("dummy");
+            User user;
+            try
+            {
+                user = storage.ReadFromFile("dummy");
+            }
+            catch (FileNotFoundException e)
+            {
+                user = new User("dummy");
+                // fill user with info 
+                FoodItem food1 = new FoodItem("Bread", "Pantry", new DateTime(2020, 07, 11), 2);
+                FoodItem food2 = new FoodItem("Peanut Butter", "Pantry", new DateTime(2020, 07, 11), 1);
+                FoodItem food3 = new FoodItem("Jelly", "Refrigerator", new DateTime(2020, 07, 11), 1);
+                FoodItem food4 = new FoodItem("Chicken", "Freezer", new DateTime(2020, 07, 11), 4);
+
+
+                user.addFoodToKitchen(food1);
+                user.addFoodToKitchen(food2);
+                user.addFoodToKitchen(food3);
+                user.addFoodToKitchen(food4);
+
+                //Recipe
+                Dictionary<string, int> ingredients = new Dictionary<string, int>()
+                {
+                    { "Bread", 2 },
+                    {"Peanut Butter", 1 },
+                    {"Jelly", 1 }
+                };
+
+                Recipe recipe = new Recipe("PB & J", ingredients, "These are the instructions.");
+
+                user.addRecipe(recipe);
+            }
             Application.Run(new Form1(user));
             
             storage.SaveToFile();
